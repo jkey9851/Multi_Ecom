34d80f50cb9a4a67c29b4850dc9a2f3495bf1f26
vh99 <jkey9851@gmail.com>
2025-12-29 13:54:19 +0700

    05: search filter

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,16 +1,44 @@
 import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
+import { SearchFilter } from './search-filter'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import { Category } from '@/payload-types'
 
 interface Props{
   children: React.ReactNode
 }
+const payload = await getPayload({
+  config: configPromise,
+})
+
+const data = await payload.find({
+  collection: "categories",
+  depth: 1, //populate subcate
+  pagination: false,
+  where: {
+    parent: {
+      exists: false,
+    },
+  },
+});
+
+const formattedData = data.docs.map((doc)=>({
+  ...doc,
+  subcategories: (doc.subcategories?.docs ?? []).map((doc)=>({
+    ...(doc as Category)
+  }))
+}))
+ 
+// console.log("data: ", data)
+// console.log("formatted data: ", formattedData)
 const Layout = ({children}:Props) => {
   return (
-
     <div>
       <div className='flex flex-col min-h-screen'>
         <Navbar></Navbar>
+        <SearchFilter data={formattedData}></SearchFilter>
         <div className='flex-1 bg-[#f4f4f0]'>
         {children}
         </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -3,21 +3,9 @@
 // import { Input } from "@/components/ui/input";
 // import { Progress } from "@/components/ui/progress";
 // import { Textarea } from "@/components/ui/textarea";
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
 export default async function Home() {
-  const payload = await getPayload({
-    config: configPromise,
-  })
-
-  const data = await payload.find({
-    collection: "categories",
-  })
-
   return (
     <div>
-        {JSON.stringify(data,null,2)}
-
         {/* <div className="flex flex-col gap-y-4 p-4">
         <div className="text-rose-600">
         Hello World
